6762caa0ee141ab20d5296e29eac2e66c5798d33
Timur G <timur91g@gmail.com>
2022-08-20 09:09:47 +0300

    add blade pages

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -12,6 +12,9 @@ class HomeController extends Controller
         <h1>Приветствие</h1>
         Some text <br />
         <a href="/">Link to Admin Page</a>
+        <a href="/about">About</a>
+        <a href="/news">News</a>
+        <a href="/categories">Categories</a>
         php;
     }
 }
