a4be0d479c428372544f14dc486b93716df0cd50
Timur G <timur91g@gmail.com>
2022-08-30 10:26:13 +0300

    add migration

---
+++ database/migrations/2022_08_28_184540_create_news_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use App\Models\News;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('news', function (Blueprint $table) {
+            $table->id();
+            $table->string('title',255);
+            $table->string('Author',150);
+            $table->enum('status',[
+                News::DRAFT, News::ACTIVE, News::BLOCKED
+            ])->default(News::DRAFT);
+            $table->dateTime('Release Day');
+            $table->text('body');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('news');
+    }
+};
